6f2aef3e3d3097301b4545e0a5c6c01ea9397ae8
Sumit Anand <anandsumit2000@gmail.com>
2024-04-09 12:10:50 +0530

    Enhanced shell file structure a little bit :)

--- eks-infra-and-application/application/deploy.sh
+++ eks-infra-and-application/application/deploy.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
 
+# Set variables
 NAMESPACE="exercise"
 REGION="us-east-1"
 CLUSTERNAME="exercise-cluster"
 APPLICATIONNAME="rattle"
 
-## Export AWS credentials of the user who
-## took the massive responsibility to 
-## deploy the EKS Cluster
-
-## Configure Kubeconfig
-aws eks update-kubeconfig --region $REGION --name $CLUSTERNAME
+# Export AWS credentials and configure kubeconfig
+aws eks update-kubeconfig --region "$REGION" --name "$CLUSTERNAME"
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to update kubeconfig."
+  exit 1
+fi
 
 # Check if the namespace exists
 if kubectl get namespace "$NAMESPACE" &> /dev/null; then
   echo "Namespace $NAMESPACE exists"
 else
-  echo "Namespace $NAMESPACE does not exist. Creating"
-  kubectl create namespace $NAMESPACE  
+  echo "Namespace $NAMESPACE does not exist. Creating..."
+  kubectl create namespace "$NAMESPACE"
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to create namespace $NAMESPACE."
+    exit 1
+  fi
 fi
 
-kubectl config set-context --current --namespace=$NAMESPACE
+# Set the namespace as default
+kubectl config set-context --current --namespace="$NAMESPACE"
+echo "Setting $NAMESPACE as the default namespace"
+echo "Done!"
 
-
-## Run helm create <applicationname> and make changes
-## to the configuration files
-
-# Check if the application directory exists
-if [ ! -d "./$APPLICATIONNAME" ]; then
-  echo "Directory ./$APPLICATIONNAME does not exist."
-  exit 1
-fi
-
-# Navigate to the ./rattle directory
-cd ./$APPLICATIONNAME || exit 1
+# Navigate to the application directory
+cd "./$APPLICATIONNAME" || { echo "Error: Directory ./$APPLICATIONNAME does not exist."; exit 1; }
 
 # Check if the Helm chart exists
-if helm list -q | grep -q "^$APPLICATIONNAME\$"; then
+if helm ls --short | grep -q "^$APPLICATIONNAME$"; then
   # Helm chart exists, perform upgrade
+  echo "Helm chart $APPLICATIONNAME already exists. Performing upgrade..."
   helm upgrade -i "$APPLICATIONNAME" .
 else
   # Helm chart does not exist, perform install
+  echo "Helm chart $APPLICATIONNAME does not exist. Performing install..."
   helm install "$APPLICATIONNAME" .
 fi
